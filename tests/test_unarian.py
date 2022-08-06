c4a666b8373551538a6278c8dcc6ec7fa0395cc8
Sam Coppini <samcoppini@gmail.com>
2022-08-06 10:47:24 -0700

    Add license

--- tests/test_unarian.py
+++ tests/test_unarian.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
 
+#
+#  Copyright 2022 Sam Coppini
+#
+#  Distributed under the Boost Software License, Version 1.0.
+#  See accompanying file LICENSE.txt or copy at
+#  http://www.boost.org/LICENSE_1_0.txt
+#
+
 import argparse
 import re
 import subprocess
